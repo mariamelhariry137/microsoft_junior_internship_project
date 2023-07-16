4fa23a2391539188cafbf3291b5da8ae976404f9
Mariam Elhariry <me@mariamelhariry.com>
2023-07-17 02:17:06 +0300

    Added js

---
+++ index.js
@@ -0,0 +1,8 @@
+const exp = require("constants");
+const express = require ("express");
+const app = express();
+
+app.listen(3000,()=> console.log("API server is running..."),);
+
+let project = [];
+
